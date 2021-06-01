8761b6404f2a5dd5e7c091f1c963faa8e76c5ef9
Ganesh J <52096566+Abiganesh@users.noreply.github.com>
2021-06-01 18:13:45 +0530

    Update antibullying.py

--- antibullying.py
+++ antibullying.py
@@ -1,13 +1,22 @@
 
 import tweepy
 from time import sleep
-
+from datetime import date
+import seaborn as sns
 import pandas as pd
+       
+import pandas as pd
+import tweepy
 
-consumer_key = "AoIo5xIosZNkYxpImVq7lH8ap"
-consumer_secret = "XUfpRVhvUJrRsJhSz9aGkPeOKjU3JS82YNmy8J4nBTpqsEcf41"
-access_token = "1366744193457000455-JQtkQxIo6iwQ7D1m2XtRtQLCAfaYP5"
-access_token_secret  = "X2cKqDhN15OR6nNe4MjeAtVx688l8NIoLOASpjbhJF7UP"
+import joblib
+
+import json
+import twitter
+import warnings
+consumer_key = "" #API
+consumer_secret = ""  #API
+access_token = ""  #API
+access_token_secret  = ""  #API
 # Access and authorize our Twitter credentials from credentials.py
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -27,19 +36,53 @@ auth.set_access_token(access_token, access_token_secret )
         # Calling api 
 api = tweepy.API(auth) 
         # 200 tweets to be extracted 
-number_of_tweets=200
+
 tweets = api.user_timeline(screen_name=username) 
         # Empty Array 
-tmp=[]  
+tmp=[]
+model_file = 'E:/academic/ganesh moodle material/6th semester/ISS/project/implementation/dataset/clean_model.csv' 
+outfile1 = 'E:/academic/ganesh moodle material/6th semester/ISS/project/implementation/dataset/tweetswhole_messages.csv'
+tmps=[] 
         # create array of tweet information: username,  
         # tweet id, date/time, text 
 tweets_for_csv = [tweet.text for tweet in tweets] # CSV file created  
 for j in tweets_for_csv: 
-            # Appending tweets to the empty array tmp 
-        tmp.append(j)  
+    texts={}
+    texts["text"]=j
+    tmp.append(texts)
+    tmps.append(j)  
         # Printing the tweets 
-        print(tmp)
+    print(tmps)
+df2=pd.DataFrame(tmp)
+result1 = [df2]
+result1 = pd.concat(result1)
+with open(outfile1, 'w') as f:
+	result1.to_csv(f, index = False)
+# Appending tweets to the empty array tmp 
+    
+data1 = pd.read_csv(outfile1)
+y = data1['text']
+whole=[]
+loaded_model1 = joblib.load(model_file)
+result1 = loaded_model1.predict(y)
+whole = result1
 
+
+poscount=0
+negcount=0
+count=0
+for i in whole:
+    count=count+1
+    if(i==1):
+        poscount=poscount+1
+    else:
+        negcount=negcount+1
+
+
+data1["result"]=result1
+print(poscount)
+print(negcount)
+print(count)
 def collect_tweets(screen_name, last_tweet, api):
 	tweets = []
 	for tweet in api.search(q=screen_name,since_id=last_tweet):
@@ -63,15 +106,7 @@ def write_tweet_messages(screen_name, last_tweet, last_message, outfile, api):
 		result.to_csv(f, index = False)
 		# header = False, columns = ['id','screen_name','text','tm','result']
         
-        
-import pandas as pd
-import tweepy
-
-import joblib
-
-import json
-import twitter
-import warnings
+ 
 
 # Access and authorize our Twitter credentials from credentials.py
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -90,8 +125,8 @@ with open("E:/academic/ganesh moodle material/6th semester/ISS/project/implement
     config_data = json.load(json_file)
 
 #print(json_data['screen_name'])
-#print(api.me().screen_name)
-#print(config_data['last_tweet'])
+#print(api.me().screen_name)NithinZahir26
+#print(config_data['last_tweet'])api.me().screen_name
 write_tweet_messages(screen_name = api.me().screen_name, last_tweet = config_data['last_tweet'], last_message = config_data['last_message'], outfile = outfile, api = api)
 data = pd.read_csv(outfile)
 model_file = 'E:/academic/ganesh moodle material/6th semester/ISS/project/implementation/dataset/clean_model.csv'
@@ -107,12 +142,12 @@ data.to_csv("E:/academic/ganesh moodle material/6th semester/ISS/project/impleme
 #message = "We have detected that you have tried to bully @"+ api.me().screen_name+". Please refrain from such behaviour in future.\nYou wrote: "
 
 message = "We have detected that you have tried to bully. Please refrain from such behaviour in future.\nYou wrote: "
-
+today=date.today()
 for i,row in data.loc[data.result == 1].iterrows():
-    if(row['tm'] == 't'):
-        sent_message = message+row['text']+" \nOn: "+str(api.get_status(row['id']).created_at)
-    else:
-        sent_message = message+row['text']+" \nOn: "+str(api.get_direct_message(row['id']).created_at)
+    
+    
+    sent_message = message+row['text']+" \nOn: "+str(today)
+  
     api.update_status("@" + row['screen_name']+"\n You should stop bullying people. (I am a bot in testing, don't take this too seriously)",row['id'])
     #api.update_status("@" + row['screen_name']+"\n You should stop bullying people. (I am a bot in testing, don't take this too seriously)",row['id'])
     print(row['screen_name'], " ", sent_message)
